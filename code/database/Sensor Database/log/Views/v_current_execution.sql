fe11854e6385b41f4f651de4b3d334f62a08c368
Mike D <mike@eondream.com>
2016-10-02 23:10:38 +0800

    added a couple of views to the log schema to make viewing the execution log more convenient

---
+++ code/database/Sensor Database/log/Views/v_current_execution.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [log].[v_current_execution]
+AS
+SELECT [Audit_key],
+	[Task_name],
+	[File_name],
+	[Processing_Start],
+	[Processing_end],
+	DATEDIFF(mi,[Processing_Start],COALESCE([Processing_end],GETDATE())) as Dur_minute
+	--DATEDIFF(hh,[Processing_Start],COALESCE([Processing_end],GETDATE())) as dur_hour
+FROM [log].[etl_executions]
+WHERE [File_name] IS NULL OR [Processing_end] IS NULL;
---
+++ code/database/Sensor Database/log/Views/v_etl_executions.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [log].[v_etl_executions]
+AS
+SELECT [Audit_key],
+	[Task_name],
+	[File_name],
+	[Processing_Start],
+	[Processing_end],
+	DATEDIFF(mi,[Processing_Start],COALESCE([Processing_end],GETDATE())) as Duration_minutes,
+	--DATEDIFF(hh,[Processing_Start],COALESCE([Processing_end],GETDATE())) as dur_hour,
+	[Items_processed],
+	[Items_successful],
+	[Items_error],
+	[Success],
+	[Row_count_start],
+	[Row_count_end],
+	[Execution_GUID]
+FROM [log].[etl_executions];
